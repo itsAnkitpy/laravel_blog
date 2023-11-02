87ad5dee00623d9e25b994cd7f7200175de13d17
itsAnkitpy <thegoat3585@gmail.com>
2023-11-02 12:24:47 +0530

    Introduced new blade components along with basic JS to integrate frontend design.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,22 +1,14 @@
 <x-layout>
 
-    <?php foreach($posts as $post): ?>
-    <article>
-        <h1>
-            <a href="/posts/<?= $post->slug ?>">
-                <?= $post->title ?>
-            </a>
-            <br><br>
+    @include('_posts-header')
 
-            <a href="/categories/{{$post->category->slug}}">{{ $post->category->name }}</a>
-            <p>By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a></p>
-
-            <div>
-                <?= $post->excerpt ?>
-            </div>
-        </h1>
-    </article>
-    <?php endforeach; ?>
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if($posts->count())
+            <x-posts-grid :posts="$posts"/>
+        @else
+            <p class="text-center">No posts to show yet.</p>
+        @endif
+    </main>
 
 </x-layout>
 
--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,9 @@ Route::get('/', function () {
     // $posts = Post::all();
     return view('posts', [
         'posts' => $posts,
+        'categories' => Category::all(),
     ]);
-});
+})->name('home');
 
 Route::get('/posts/{post:slug}', function (Post $post){
 
@@ -24,12 +25,15 @@ Route::get('/posts/{post:slug}', function (Post $post){
 
 Route::get('categories/{category:slug}', function (Category $category){
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts,
+        'categories' => Category::all(),
+        'currentCategory' => $category, 
     ]);
-});
+})->name('category');
 
 Route::get('authors/{author:username}', function (User $author){
     return view('posts', [
-        'posts' => $author->posts
+        'posts' => $author->posts,
+        'categories' => Category::all(),
     ]);
 });
