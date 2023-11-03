8c44777c944f4615e505e99bed69582e23c79763
itsAnkitpy <thegoat3585@gmail.com>
2023-11-03 13:39:07 +0530

    Merged the search and category queries and fixed a search bug of AND query

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-use App\Models\Category;
-use App\Models\User;
 use App\Http\Controllers\PostController;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
-
-Route::get('authors/{author:username}', function (User $author){
-    return view('posts', [
-        'posts' => $author->posts,
-    ]);
-});
