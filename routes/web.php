9142903a489acc25471fd0ce9afa0252ec1f1d6b
itsAnkitpy <thegoat3585@gmail.com>
2023-11-08 14:10:54 +0530

    Added Login and Logout functionality.

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,19 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionsController;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
 // Register routes
-Route::get('register', [RegisterController::class, 'create']);
-Route::post('register', [RegisterController::class, 'store']);
+Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
+Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
+
+// Login routes
+Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
+Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
+
+// Logout routes
+Route::post('logout', [SessionsController::class, 'destory'])->middleware('auth');
 
